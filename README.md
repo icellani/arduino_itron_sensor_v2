fbd5e479f5db7bf0487e89f4ecdd193117b8ef9d
Igor Cellani <igor@cellani.com.br>
2017-11-04 17:44:21 -0200

    Innitial Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Arduino Itron Sensor v2
+
+Código fonte para integração do Itron Sensor v2 com o Arduino.
---
+++ src/arduino_itron_sensor_v2.ino
@@ -0,0 +1,39 @@
+#include <EtherCard.h>
+
+int val = 0;
+int contZero = 0;
+int ciclo = 0;
+
+const int analogInPin = A0;
+
+void setup() {
+
+  Serial.begin( 9600 );
+}
+
+void loop(){
+
+  val = analogRead( analogInPin );
+
+  if ( contZero == 5 ) {
+
+    ciclo = ciclo + 1;
+
+    Serial.println();
+    Serial.print( "Ciclo: " );
+    Serial.print( ciclo );
+
+    contZero = 0;
+  }
+
+  if ( val == 0 ) {
+
+    contZero = contZero + 1;
+
+  } else {
+
+    contZero = 0;
+  }
+
+  delay( 10 );
+}
